8fdb289fb1e3a943bfe3aa13bdd8066fe41ded9d
Pavel Pavlov <Pavel.Pavlov@endava.com>
2023-03-19 21:36:50 +0200

    tests added

---
+++ Bookify/Bookify.ApiTests/Base.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using RestSharp;
+
+
+namespace Bookify.ApiTests
+{
+    public class Base
+    {
+        protected RestClient client;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            var clientOptions = new RestClientOptions()
+            {
+                BaseUrl = new Uri("https://localhost:7095/"),
+                Authenticator = null,
+            };
+            client = new RestClient(clientOptions);
+        }
+
+        [OneTimeTearDown]
+        public void tearDown()
+        {
+
+        }
+    }
+}
---
+++ Bookify/Bookify.ApiTests/TestData/TestData.cs
@@ -0,0 +1,19 @@
+﻿using Bookify.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.ApiTests.TestData
+{
+    public class TestData
+    {
+        public static BookInputModel DefauldBookData = new BookInputModel
+        {
+            Title = "Test Book",
+            PagesCount = 100,
+            AuthorId = Guid.Parse("2788DCBA-EEBF-4C87-9D73-35DD187C2277")
+        };
+    }
+}
